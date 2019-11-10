aded70a9f11ab063f0ff042d5831c7f0bc78126a
Alex Chow <a.chow@wustl.edu>
2019-11-10 17:13:20 -0600

    Moved Lab3 main into Lab4 for now

--- Lab4/Lab4.cpp
+++ Lab4/Lab4.cpp
@@ -1,13 +1,33 @@
 // Lab4.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
+#include <string>
+#include "exitCodes.h"
+#include "GameBoard.h"
+using namespace std;
+int usageMsg(char* str);
 
-int main()
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!\n";
+	string s = "TicTacToe";
+	if (argc != indices::numberOfArgs) {
+		return usageMsg(argv[indices::programName]);
+	}
+
+	else if (argv[indices::inputName] == s) {
+		TicTacToe ttt;
+		int gameState = ttt.play();
+		return gameState;
+	}
+	else {
+		return usageMsg(argv[indices::programName]);
+	}
 }
 
+int usageMsg(char* str) {
+	cout << "usage:" << str << " <game name (eg, TicTacToe)>" << endl;
+	return wrongNumOfArgs;
+}
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
